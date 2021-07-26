4c35e858c1d03c8c25919976f4c3c8f600cff93f
Mrunmay1312 <55950819+Mrunmay1312@users.noreply.github.com>
2021-07-26 19:22:38 +0530

    Update TestCase1.java

--- src/test/TestCase1.java
+++ src/test/TestCase1.java
@@ -13,7 +13,7 @@ public class TestCase1 {
 	public void setup() throws InterruptedException
 	{
 		// Start chrome browser
-		System.setProperty("webdriver.chrome.driver", "test-output/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Project/Job-3 Testing using selenium/chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		
 		driver.manage().window().maximize();
